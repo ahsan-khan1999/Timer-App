fc3f8cffd4426692355250c81f112f521c50604d
ahsan-khan1999 <ahsan.ak943@gmail.com>
2020-10-04 18:17:09 +0500

    some styling and button alignment

--- src/Timer.js
+++ src/Timer.js
@@ -2,12 +2,12 @@ import React, { useState, useRef } from 'react'
 
 export default function Timer() {
     const [time, setTime] = useState(0);
-    let [isOn, setIsOn] = useState(false);
+    // let [isOn, setIsOn] = useState(false);
     let [interva, setInterva] = useState();
     let intervalId = useRef();
     const startTime = () => {
         // run();
-        setIsOn(true);
+        // setIsOn(true);
         const now = Date.now();
         setInterva(setInterval(() => {
             return setTime((Date.now() - now) / 1000)
@@ -15,18 +15,12 @@ export default function Timer() {
 
     }
     const stopTime = () => {
-
+        clearInterval(interva);
     }
     const resetTime = () => {
         clearInterval(interva);
         setTime(0)
-        
-        console.log(time,interva);
     }
-    var updatedS = time.s, updatedM = time.m, updatedH = time.h;
-    // const run = () => {
-    //     return setTime();
-    // }
     return (
         <div>
             <h1>Timer App : </h1>
@@ -36,7 +30,7 @@ export default function Timer() {
 
                 <div>
                     <button onClick={startTime} style={{ border: '3px solid purple', marginTop: 20, marginRight: 100, height: 50, width: 80, fontSize: 22, backgroundColor: 'pink' }}>Start</button>
-                    <button style={{ border: '3px solid purple', height: 50, width: 80, fontSize: 22, backgroundColor: 'pink' }}>Stop</button>
+                    <button onClick={stopTime} style={{ border: '3px solid purple', height: 50, width: 80, fontSize: 22, backgroundColor: 'pink' }}>Stop</button>
                     <button onClick={resetTime} style={{ border: '3px solid purple', height: 50, width: 80, fontSize: 22, backgroundColor: 'pink' }}>Reset</button>
                 </div>
             </div>
